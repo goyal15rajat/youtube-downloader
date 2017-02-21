bc005c0d8a6c418daad19c3d545ea7a10e7c801c
goyal15rajat <goyal.rajat15@gmail.com>
2017-02-22 03:14:29 +0530

    update crawlurl
    added download all playlist function

--- crawlurl.py
+++ crawlurl.py
@@ -12,9 +12,9 @@ class crawlUrl(object):
 	def __init__(self,urltype,htmlurl):
 		self.htmlUrl = htmlurl
 		self.urlType = urltype
-		self.dict_standalone={}
-		self.dict_playlist={}
+		self.video_list={}
 		self.downloads={}
+		self.title_list={}
 
 	def crawlQuery(self):
 		req = self.requestUrl(self.htmlUrl)
@@ -28,34 +28,32 @@ class crawlUrl(object):
 
 	def crawlQueryUrl(self,req,):
 		soup = BeautifulSoup(urlopen(req).read(),"html.parser")
-
-		dict_standalone={}
-		dict_playlist={}
 		stop_words = (['"', "'",'/'])
 		#Parsing web urls
 		for item in soup.find_all('h3', attrs={'class' : 'yt-lockup-title'}):
 
 		    link = item.a['href']
 		    print (link)
-		    full_link ="https://www.youmagictube.com"+link
 		    title = item.a['title']
 		    title = [i for i in title if i not in stop_words]
 		    title = "".join(title)
 		    if self.urlType == 2:
 		    	if "&list=" in link:
-		    		self.dict_playlist[title]=full_link
+		    		full_link ="https://www.youtube.com"+link
+		    		self.video_list[title]=full_link
 		    elif self.urlType == 1:
+		    	full_link ="https://www.youmagictube.com"+link
 		    	if "&list=" in link:
 		    		continue
 		    	else:
-		    		self.dict_standalone[title]=full_link
+		    		self.video_list[title]=full_link
 		if  self.urlType == 1:
-			return (self.dict_standalone)
+			return (self.video_list)
 		elif self.urlType == 2:
-			return(self.dict_playlist)
+			return(self.video_list)
 
-	def selectLink(elf,videos_choice):
-		choice = self.dict_standalone[videos_choice]
+	def selectLink(self,videos_choice):
+		choice = self.video_list[videos_choice]
 		quality = self.findQuality(choice)
 		return(quality)
 	def findQuality(self,videoslink):		
@@ -110,4 +108,22 @@ class crawlUrl(object):
 			r = requests.get(temp1)
 			target = urllib.request.urlretrieve(r.history[0].url,download_name)
 
+	def getPlaylist_videos(self,playlist_choice):
+		title_list={}
+		choice = self.video_list[playlist_choice]
+		req = self.requestUrl(choice)
+		soup = BeautifulSoup(urlopen(req).read(),"html.parser")
+		divs = soup.find('div', attrs={'class' : 'playlist-videos-container yt-scrollbar-dark yt-scrollbar'})
+		for item in divs.find_all('li', attrs={'class' : 'yt-uix-scroller-scroll-unit'}):
+			self.title_list[item['data-video-title']] = item['data-video-id']
+		print(self.title_list)
 
+	def downloadallPlaylist(self):
+		for keys in self.title_list:
+			q = self.findQuality(self.title_list[keys])
+			quality=list(self.downloads.keys())
+			quality = "' FALSE '".join(quality)	
+			quality_choice=subprocess.getstatusoutput("zenity --title='video quality' --list --radiolist --column '' --column 'video quality' FALSE 'default' FALSE '"+quality+"'")[1].split("\n")[1]
+			self.crawlQualitylink(quality_choice)
+			print("DONE")
+			
